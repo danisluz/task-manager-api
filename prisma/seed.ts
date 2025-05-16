aab360da5d36c0b5b1feb4cceb9cb3c8b9b01b6a
daniel.luz_t <daniel.luz_t@ti.growdev.com.br>
2025-05-16 00:17:27 -0300

    Add roles, groups, and enhanced superuser seeding logic
    Integrated default roles and a user group in the seed process. Updated Docker Compose for better environment variable configuration and improved PostgreSQL setup. These changes streamline user management and development setup.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,30 +7,62 @@ dotenv.config();
 const prisma = new PrismaClient();
 
 async function main() {
-  // Pegando dados do arquivo .env
+  // Variáveis de ambiente para o superusuário
   const email = process.env.SUPERUSER_EMAIL || 'admin@system.com';
   const password = process.env.SUPERUSER_PASSWORD || '123456';
 
-  // Criptografando a senha
+  // Hash da senha do superusuário
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Criando ou atualizando o usuário administrador
-  const user = await prisma.user.upsert({
+  // Criar Roles Padrão
+  const adminRole = await prisma.role.upsert({
+    where: { name: 'ADMIN' },
+    update: {},
+    create: { name: 'ADMIN' },
+  });
+
+  const userRole = await prisma.role.upsert({
+    where: { name: 'USER' },
+    update: {},
+    create: { name: 'USER' },
+  });
+
+  // Criar Grupo Padrão
+  const adminGroup = await prisma.userGroup.upsert({
+    where: { name: 'AdminGroup' },
+    update: {},
+    create: { name: 'AdminGroup' },
+  });
+
+  console.log('Roles e grupos criados com sucesso.');
+
+  // Criar ou atualizar o superusuário
+  const superUser = await prisma.user.upsert({
     where: { email },
     update: {},
     create: {
       name: 'Super Admin',
       email,
       password: hashedPassword,
+      roles: {
+        create: {
+          role: {
+            connect: { id: adminRole.id },
+          },
+        },
+      },
+      userGroups: {
+        connect: { id: adminGroup.id },
+      },
     },
   });
 
-  console.log(`Usuário principal criado/atualizado: ${user.email}`);
+  console.log(`Superusuário criado/atualizado: ${superUser.email}`);
 }
 
 main()
   .catch((e) => {
-    console.error(e);
+    console.error('Erro ao criar o superusuário:', e);
     process.exit(1);
   })
   .finally(async () => {
